9dc73a1436d72edab61bfb0d0aa73b66e779da04
Aravind Kumar Jagannathan <Aj@aravind-kumar-jagannathan.local>
2016-11-26 19:34:53 -0600

    Initial Commit

---
+++ versionmgmt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  versionmgmt
+//
+//  Created by Aravind Kumar Jagannathan on 11/26/16.
+//  Copyright © 2016 big nerd ranch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
